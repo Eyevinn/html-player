2e72a60fa6f856f57764687c3be953b2de62af92
birme <jonas.birme@eyevinn.se>
2018-04-07 10:10:34 +0200

    Added test framework

---
+++ spec/player_tech_factory_spec.js
@@ -0,0 +1,7 @@
+//const PlayerTechFactory = require('../src/player_tech_factory.js');
+
+describe('Player Tech Factory', () => {
+  it('can determine correct type', done => {
+    done();
+  });
+});
