38cd667d2a2462660181208defcc358fed59ab9d
Erik Hoffman <erik.johansson87@gmail.com>
2020-05-12 11:47:43 +0200

    npm publishing (#10)
    * org scoped the package name
    
    * separate init file and builds to have the object exported instead of on window object
    
    * updated ignore files
    
    * updated readme with npm related instructions
    
    * publish workflow for github actions
    
    * some simplifications regarding two entry files

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-import { Player } from "./src/player";
-import { DEFAULT_OPTIONS } from "./src/utils/constants";
+import { setupEyevinnPlayer } from "./main.js";
 
-window.setupEyevinnPlayer = async function(wrapperId, manifestUrl, opts) {
-  const options = Object.assign({}, DEFAULT_OPTIONS, opts);
-  const player = new Player(wrapperId, manifestUrl, options);
-  return await player.init();
-};
+window.setupEyevinnPlayer = setupEyevinnPlayer;
