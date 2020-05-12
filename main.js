fd83e1ad316a4a8dfdd493d058a69e95ef1fb152
Erik Hoffman <erik.johansson87@gmail.com>
2020-05-12 10:55:51 +0200

    separate init file and builds to have the object exported instead of on window object

---
+++ main.js
@@ -0,0 +1,8 @@
+import { Player } from "./src/player";
+import { DEFAULT_OPTIONS } from "./src/utils/constants";
+
+export const setupEyevinnPlayer = async function(wrapperId, manifestUrl, opts) {
+  const options = Object.assign({}, DEFAULT_OPTIONS, opts);
+  const player = new Player(wrapperId, manifestUrl, options);
+  return await player.init();
+};
